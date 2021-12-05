00c8a46b57f3a9e554dc3c8d2ce95fe936d49b43
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2021-12-04 20:06:52 -0700

    bug fixes

--- thieving-skills-for-bards/lua/M_B3THIE.lua
+++ thieving-skills-for-bards/lua/M_B3THIE.lua
@@ -2,10 +2,10 @@ function EEex_Actionbar_Hook_HasFullThieving(sprite)
     return true
 end
 
-function MOBardThievingActionbarListener(config, state)
+function B3BardThievingActionbarListener(config, state)
    if state == 5 then
       EEex_Actionbar_SetButton(4, EEex_Actionbar_ButtonType.FIND_TRAPS)
       EEex_Actionbar_SetButton(5, EEex_Actionbar_ButtonType.THIEVING)
    end
 end
-EEex_Actionbar_AddListener(MOBardThievingActionbarListener)
+EEex_Actionbar_AddListener(B3BardThievingActionbarListener)
