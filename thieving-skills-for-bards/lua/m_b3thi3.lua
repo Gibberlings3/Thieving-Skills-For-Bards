3bba3b3a37ceb6e70e2c0c08f514e211e1d184e6
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2022-01-15 13:35:52 -0700

    Detect Illusion for Ranger

--- thieving-skills-for-bards/lua/m_b3thi3.lua
+++ thieving-skills-for-bards/lua/m_b3thi3.lua
@@ -1,7 +1,6 @@
 function B3RangerThievingActionbarListener(config, state)
    if state == 12 then
-      EEex_Actionbar_SetButton(3, EEex_Actionbar_ButtonType.FIND_TRAPS)
-      EEex_Actionbar_SetButton(4, EEex_Actionbar_ButtonType.THIEVING)
+      EEex_Actionbar_SetButton(4, EEex_Actionbar_ButtonType.FIND_TRAPS)
    end
 end
 EEex_Actionbar_AddListener(B3RangerThievingActionbarListener)
