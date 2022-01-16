372f4ed96d8b25998d07a5881f3a7e759d1c1444
morpheus562 <81997843+morpheus562@users.noreply.github.com>
2022-01-15 17:14:08 -0700

    Cleric/Ranger

--- thieving-skills-for-bards/lua/m_b3thi3.lua
+++ thieving-skills-for-bards/lua/m_b3thi3.lua
@@ -1,5 +1,5 @@
 function B3RangerThievingActionbarListener(config, state)
-   if state == 12 then
+   if state == 12 or state == 18 then
       EEex_Actionbar_SetButton(4, EEex_Actionbar_ButtonType.FIND_TRAPS)
    end
 end
